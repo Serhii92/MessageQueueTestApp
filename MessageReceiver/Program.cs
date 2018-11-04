15525d6400b5684f13e889a73524416ea343022e
DESKTOP-NBA1BI2\Admin <nizhnichenko92@gmail.com>
2018-11-04 22:30:29 +0200

    initial commit

---
+++ MessageReceiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MessageReceiver
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("This is receiver application!");
+
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(queue: "msgKey",
+					durable: false,
+					exclusive: false,
+					autoDelete: false,
+					arguments: null);
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body;
+					var message = Encoding.UTF8.GetString(body);
+					Console.WriteLine($" -- Received {message}");
+				};
+				channel.BasicConsume(queue: "msgKey",
+					autoAck: true,
+					consumer: consumer);
+				Console.WriteLine("Press enter to exit");
+				Console.ReadLine();
+			}
+
+
+		}
+	}
+}
---
+++ MessageSender/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MessageSender
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("This is sender application");
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(queue: "msgKey",
+					durable: false,
+					exclusive: false,
+					autoDelete: false,
+					arguments: null);
+				Console.WriteLine("Enter message to send");
+				var msg = "";
+				do
+				{
+					msg = Console.ReadLine();
+					if (!string.IsNullOrEmpty(msg))
+					{
+						SendMessage(channel, msg);
+					}
+					else
+					{
+						Console.WriteLine("Empty input, please try again");
+					}
+				} while (string.IsNullOrEmpty(msg));
+			}
+
+			Console.WriteLine("Press enter to exit");
+			Console.ReadLine();
+		}
+
+		public static void SendMessage(IModel channel, string message)
+		{
+			var body = Encoding.UTF8.GetBytes(message);
+			channel.BasicPublish(exchange: "",
+				routingKey: "msgKey",
+				basicProperties: null,
+				body: body);
+			Console.WriteLine($" -- Sent {message}");
+		}
+	}
+}
